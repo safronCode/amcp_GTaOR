f2a55095c49beb3fa9afca9a363e7cad833aa5c0
limett17 <92600020+limett17@users.noreply.github.com>
2025-02-20 12:44:41 +0300

    Complete new version of BnB algorithm
    - The priority queue was added: Now the algorithm goes first through those branches, that have the most potential to have optimal solution
    
    - The algorithm now uses the scipy.optimize linprog method to calculate estimation for the branches.

--- SimplexMethod/package/methods/branch_bound.py
+++ SimplexMethod/package/methods/branch_bound.py
@@ -1,8 +1,9 @@
 import numpy as np
-from .simplex_method import *
+from scipy.optimize import linprog
+import heapq
 
 
-def branch_and_bound(c, A, b, sign):
+def branch_and_bound(c, A, b, bounds):
     """
     Решение задачи целочисленного линейного программирования методом ветвей и границ.
 
@@ -10,52 +11,51 @@ def branch_and_bound(c, A, b, sign):
     - c: коэффициенты целевой функции (1D массив).
     - A: матрица ограничений (2D массив).
     - b: правая часть ограничений (1D массив).
-    - sign: строка знаков ограничений (1D массив).
+    - bounds: список пар (min, max) для переменных (список кортежей).
 
     Возвращает:
-    - Оптимальное значение целевой функции (или None, если нет решения).
+    - Оптимальное значение целевой функции.
     - Оптимальный вектор решений (или None, если нет решения).
     """
-    check_point = np.zeros(np.size(c))
-    for mode in [1, -1]:
-        point, fval, _ = simplex_method(mode * c, A, b, sign)
-        check_point = np.vstack([check_point, point])
-    check_point = np.delete(check_point, (0), axis=0)
-
-    if not ((np.abs(np.floor(check_point[0]) - np.floor(check_point[1])) >= 1).all()):
-        print('Задача не имеет целочисленных решений')
-        return None, None
-
-    queue = [(c, A, b, sign)]
+    queue = []
     best_solution = None
     best_value = -np.inf
 
+    res = linprog(c, A_ub=A, b_ub=b, bounds=bounds, method='highs')
+    #print("linprog status:", res.status)  # 2 - нет решений, 3 - неограниченная
+
+    if res.success:
+        heapq.heappush(queue, (-res.fun, res.x, A, b))  # Приоритет -res.fun для max heap
+
     while queue:
-        current_c, current_A, current_b, current_sign = queue.pop(0)
-        res = simplex_method(current_c, current_A, current_b, current_sign)
-        x = res[0]
-        if np.allclose(x, np.round(x)):
-            value = res[1]
+        _, x, current_A, current_b = heapq.heappop(queue)  # Извлекаем узел с наибольшим потенциалом
+        res = linprog(c, A_ub=current_A, b_ub=current_b, bounds=bounds, method='highs')
+
+        if np.all(x == np.floor(x)):
+            value = -res.fun
             if value > best_value:
                 best_value = value
                 best_solution = x
-            break
+            continue
 
-        else:
-            fractional_index = np.argmax(np.abs(x - np.round(x)))
-            fractional_value = x[fractional_index]
+        if res.fun <= best_value:
+            continue
 
-            new_sign = np.append(current_sign, 1)
+        fractional_index = np.argmax(x - np.floor(x))
+        fractional_value = x[fractional_index]
 
-            lower_A = np.vstack([current_A, np.zeros(len(current_c))])
-            lower_A[-1, fractional_index] = 1
-            lower_b = np.append(current_b, np.floor(fractional_value))
+        lower_A = np.vstack([current_A, np.zeros(len(c))])
+        lower_A[-1, fractional_index] = 1  # x[fractional_index] <= floor(fractional_value)
+        lower_b = np.append(current_b, np.floor(fractional_value))
 
-            upper_A = np.vstack([current_A, np.zeros(len(current_c))])
-            upper_A[-1, fractional_index] = -1
-            upper_b = np.append(current_b, -np.ceil(fractional_value))
+        upper_A = np.vstack([current_A, np.zeros(len(c))])
+        upper_A[-1, fractional_index] = -1  # x[fractional_index] >= ceil(fractional_value)
+        upper_b = np.append(current_b, -np.ceil(fractional_value))
 
-            queue.append((current_c, lower_A, lower_b, new_sign))
-            queue.append((current_c, upper_A, upper_b, new_sign))
+        for new_A, new_b in [(lower_A, lower_b), (upper_A, upper_b)]:
+            res = linprog(c, A_ub=new_A, b_ub=new_b, bounds=bounds, method='highs')
+            #print("linprog status:", res.status)  # 2 - нет решений, 3 - неограниченная
+            if res.success:
+                heapq.heappush(queue, (-res.fun, tuple(res.x), new_A, new_b))
 
-    return best_solution, -best_value
+    return best_value, best_solution
